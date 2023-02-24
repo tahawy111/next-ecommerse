c469eb1a6c9d89cfdc7254e0622b9f8eaa31f8bf
tahawy111 <amertahawy111@gmail.com>
2023-02-24 18:14:48 +0200

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="Next ECommerce Website By Amer TAHAWY"
+        />
+        <meta name="keywords" content="Products Phones, Laptops, and more" />
+        <meta name="author" content="Amer Tahawy" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
