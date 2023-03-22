3706655d7851f044ec2209d44e1675f6aacbf309
tahawy111 <amertahawy111@gmail.com>
2023-03-22 08:57:43 +0200

    .

--- components/product/ProductItem.tsx
+++ components/product/ProductItem.tsx
@@ -4,10 +4,11 @@ import React from 'react'
 interface IProps {
     product: IProductModel;
 }
-const ProductItem = ({ product }: IProps) => {
+const ProductItem = ({ product }: IProps,...props: any[]) => {
     return (
+        <div {...props}>
         <div className="card w-80 bg-base-100 shadow-xl">
-            <figure><img className='object-cover w-80 h-80' src={product.images[0].url} alt={product.images[0].url} /></figure>
+            <figure><img className='object-cover w-80 h-80 2xl:w-64 2xl:h-64' src={product.images[0].url} alt={product.images[0].url} /></figure>
             <div className="card-body">
                 <h2 className="card-title capitalize">{product.title}</h2>
                 <div className='flex justify-between'>
@@ -24,6 +25,7 @@ const ProductItem = ({ product }: IProps) => {
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -3,12 +3,19 @@ import { IProductModel } from '@/models/Product'
 import { BASE_URL, capitalize } from '@/utils/globals'
 import axios from 'axios'
 import Head from 'next/head'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 interface IProps {
     product: IProductModel;
 }
 const DetailProduct = ({ product }: IProps) => {
-    const [imgIndex,setImgIndex] = useState(0)
+    const [imgIndex, setImgIndex] = useState(0);
+    const [imgIndexTransform, setImgIndexTransform] = useState(0);
+    const divImagesRef = useRef<HTMLImageElement>(null)
+
+    useEffect(() => {
+        console.log(divImagesRef.current.naturalWidth)
+    },[])
+
     return (
         <Layout>
             <Head>
@@ -18,12 +25,23 @@ const DetailProduct = ({ product }: IProps) => {
             </Head>
 
             <div className='grid grid-cols-2 gap-x-3'>
-                <div className="flex items-center">
-                    <div className="w-fit mx-3">                            {product.images.map(({ url }, index) => (
-                        <img key={index} className={`w-40 border border-[#ddd] rounded-lg p-1 hover:border-black dark:border-black dark:hover:border-[#ddd] ${index === imgIndex && "border-black"}`} onClick={() => setImgIndex(index)} src={url} alt={url} />
-                    ))}</div>
-                    <div>
-                        <img className='border border-[#ddd] rounded-lg p-1 w-full object-cover' src={product.images[imgIndex].url} alt={product.images[imgIndex].url} />
+                <div className="grid">
+
+                    <div className={`w-[600px]`}>
+                        <img className='border border-[#ddd] rounded-lg p-1 w-full h-[520px]' src={product.images[imgIndex].url} alt={product.images[imgIndex].url} />
+                        <div className={`flex mx-3 items-center justify-center`}>
+                            <span className={`material-icons-outlined cursor-pointer select-none ${product.images.length < 7 && "pointer-events-none opacity-50"}`} onClick={() => setImgIndexTransform((prev) => prev + 1 > 0 ? 0 : prev + 1)}>
+                                arrow_back_ios
+                            </span>
+                            <div className={`flex overflow-hidden transition-transform duration-150 gap-x-3`}>
+                            {product.images.map(({ url }, index) => (
+                                <img ref={divImagesRef} style={{transform:`translateX(${imgIndexTransform * 100}px)`}} key={index} className={`transition-transform duration-150 h-[80px] w-[80px] cursor-pointer object-cover border border-[#ddd] rounded-lg p-1 hover:border-black dark:border-black dark:hover:border-[#e0ac3bcc] ${index === imgIndex && "border-black dark:border-[#ddd]"}`} onClick={() => setImgIndex(index)} src={url} alt={url} />
+                            ))}
+                            </div>
+                            {/* <span className={`material-icons-outlined cursor-pointer select-none ${product.images.length < 7 && "pointer-events-none opacity-50"}`} onClick={() => setImgIndexTransform((prev) => prev - 1 < divImagesRef.current?.naturalWidth ? 0 : 1 )}> */}
+                                {/* arrow_forward_ios */}
+                            {/* </span> */}
+                        </div>
                     </div>
                 </div>
                 <div className="">Hello</div>
